5f776df729b669757637e5574e2768b732fafe6f
Fisher <32707008+FisherWY@users.noreply.github.com>
2019-05-28 23:55:07 +0800

    Update

---
+++ Final/js/schedule.js
@@ -0,0 +1,7 @@
+function addToCalendar() {
+	window.location = "https://www.stanza.co/button/launch?calendar=https%3A%2F%2Fspoton.it%2Fschedules%2Fnba%2Fspurs&size=medium&options=ticker#/subscribe/nba-spurs"
+}
+
+function printableSchedule() {
+	window.location = "https://www.nba.com/.element/media/2.0/teamsites/spurs/1819_Spurs_Calendar.pdf"
+}
